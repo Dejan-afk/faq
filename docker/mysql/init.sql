c3a4cf92c92666f5ebea5518b9ef5d6618cf8757
Dejan-afk <dejanfra@web.de>
2025-07-13 14:43:42 +0200

    Added testing db with test for authentication

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,4 +1,9 @@
 CREATE DATABASE IF NOT EXISTS faq_app CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS faq_app_testing CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
 CREATE USER IF NOT EXISTS 'faq_user'@'%' IDENTIFIED BY 'neo';
+
 GRANT ALL PRIVILEGES ON faq_app.* TO 'faq_user'@'%';
+GRANT ALL PRIVILEGES ON faq_app_testing.* TO 'faq_user'@'%';
+
 FLUSH PRIVILEGES;
