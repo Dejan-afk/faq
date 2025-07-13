f2abf547ac0865478693e4b34b441976740081f2
Dejan-afk <dejanfra@web.de>
2025-07-13 15:11:58 +0200

    Added pages skeletons

---
+++ resources/js/Pages/Admin/Faq.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="faq-admin">
+        <h1>FAQ Admin Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FaqAdmin',
+};
+</script>
+
+<style scoped>
+.faq-admin {
+    padding: 2rem;
+}
+</style>
---
+++ resources/js/Pages/Admin/Tag.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="tag-admin">
+        <h1>Tag Administration</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TagAdmin',
+};
+</script>
+
+<style scoped>
+.tag-admin {
+    padding: 1rem;
+}
+</style>
