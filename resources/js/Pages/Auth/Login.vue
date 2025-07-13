4162b8ca91fa2221c4a60e47121944b55bdf4ff7
Dejan-afk <dejanfra@web.de>
2025-07-13 14:44:19 +0200

    Added Vue Setup

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { reactive } from 'vue'
+import { useForm, Head, Link } from '@inertiajs/vue3'
+
+defineProps({
+    status: {
+        type: String,
+    },
+});
+
+const form = useForm({
+    email: '',
+    password: '',
+    remember: false,
+});
+
+const submit = () => {
+    form.post(route('login'), {
+        onFinish: () => form.reset('password'),
+    });
+};
+</script>
+
+<template>
+  <form @submit.prevent="submit">
+    <input v-model="form.email" placeholder="Email" />
+    <input v-model="form.password" type="password" placeholder="Passwort" />
+    <button type="submit">Login</button>
+  </form>
+</template>
---
+++ resources/js/Pages/Faq/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>FAQ Übersicht</h1>
+
+    <ul>
+      <li v-for="faq in faqs" :key="faq.id">
+        <strong>{{ faq.question }}</strong><br>
+        <span>{{ faq.answer }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+    defineProps({
+        faqs: Array,
+    });
+</script>
