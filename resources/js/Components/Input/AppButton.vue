3c3352004ea2da589c31158c53c05c521f5c3e8e
Dejan-afk <dejanfra@web.de>
2025-07-18 13:11:55 +0200

    Added new variant for AppButton

--- resources/js/Components/Input/AppButton.vue
+++ resources/js/Components/Input/AppButton.vue
@@ -6,16 +6,33 @@
     :class="[baseClass, variantClass, props.class]"
     @click="onClick"
   >
+    <!-- grüner Kreis-Wrapper -->
+    <div
+      v-if="variant === 'dark-icon'"
+      class="app-button-icon-circle"
+    >
+      <SvgIcon
+        v-if="icon"
+        :src="icon"
+        :wrapper-class="iconClass"
+        :style="rotateIconStyle"
+      />
+    </div>
+
+    <!-- Normaler Icon-Wrapper -->
     <SvgIcon
-      v-if="icon"
+      v-else-if="icon"
       :src="icon"
       :wrapper-class="iconClass"
+      :style="rotateIconStyle"
     />
+
     <span>{{ label }}</span>
   </component>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 import SvgIcon from '@/Components/SvgIcon.vue'
 import '../../../css/app-button.css'
 
@@ -25,7 +42,8 @@ const props = defineProps({
   href: { type: String, default: null },
   as: { type: String, default: 'button' }, // 'a' oder 'button'
   type: { type: String, default: 'button' },
-  variant: { type: String, default: 'primary' }, // 'primary' | 'secondary'
+  variant: { type: String, default: 'primary' }, // 'primary' | 'secondary' | 'dark'
+  rotateIcon: { type: [String, Number], default: null },
 })
 
 const emit = defineEmits(['click'])
@@ -35,4 +53,9 @@ const onClick = (e) => emit('click', e)
 const baseClass = 'app-button'
 const iconClass = `app-button-icon--${props.variant}`
 const variantClass = `app-button--${props.variant}`
+
+const rotateIconStyle = computed(() =>
+  props.rotateIcon ? { transform: `rotate(${props.rotateIcon}deg)` } : {}
+)
+
 </script>
